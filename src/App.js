8c527b0762dd10d35a0a4d8e17103f08f9c251fe
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2019-10-22 13:39:08 -0700

    Fixed Wikipedia Search

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-import React, {Component} from 'react';
-import './App.css';
-import Character from './components/Character';
-import Search from './components/Search';
+import React, { Component } from "react";
+import "./App.css";
+import Character from "./components/Character";
+import Search from "./components/Search";
 
-const charArray =[
-  {name: 'Mark', race: 'Human', status:'Full-Health', comment:''},
-  {name: 'Goerge', race: 'Demon', status:'Rage', comment:'I am so angry'},
-  {name: 'Mark', race: 'Human', status:'Full-Health', comment:''}
-]
-
-
+const charArray = [
+  { name: "Mark", race: "Human", status: "Full-Health", comment: "" },
+  { name: "Goerge", race: "Demon", status: "Rage", comment: "I am so angry" },
+  { name: "Mark", race: "Human", status: "Full-Health", comment: "" }
+];
 
 class App extends Component {
-  listComp =() =>{
-    return charArray.map((item,index)=> ( <Character 
-      key={index}
-    name= {item.name}
-    race={item.race}
-    status={item.status}
-    comment={item.comment}
-    />
+  listComp = () => {
+    return charArray.map((item, index) => (
+      <Character
+        key={index}
+        name={item.name}
+        race={item.race}
+        status={item.status}
+        comment={item.comment}
+      />
     ));
-     
   };
-  render(){
-  return (
-    <div className="App">
- <main>
-      <Search/>
-      </main>
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <Search />
+        </main>
 
-      <header className="App-header">
+        {/* <header className="App-header">
         <h1> This is our first Webpack App</h1>
         {this.listComp()}
-      </header>
-
-     
-
-
-    </div>
-  );
+      </header> */}
+      </div>
+    );
   }
 }
 
-
 export default App;
-
-
-
